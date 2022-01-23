49c030b2ca72695c764f6b5d9e5efd630eeac7d3
KamiteAkira0611 <kamiteakira@gmail.com>
2022-01-24 02:29:09 +0900

    save

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
+/* eslint-disable import/extensions */
 import "src/styles/nprogress.css"
-import nProgress from 'nprogress';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
